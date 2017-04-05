0d5c0ee4b005f4d82d027fff0a9218ae2a62fcf4
Tiago Almeida <tiago.almeida@bluefinsolutions.com>
2017-04-05 21:14:16 +0100

    Added 2 models from Northwind to be able to compare the metadata output

--- djangoproject/webapp/models.py
+++ djangoproject/webapp/models.py
@@ -13,3 +13,13 @@ class Tag(models.Model):
 class Author(models.Model):
   name = models.CharField(max_length=1024)
   dateOfBirth = models.DateField()
+
+class CustomerDemographic(models.Model):
+	CustomerDesc = models.CharField(max_length=100)
+	Customers = models.ManyToManyField('Customer', 
+		related_name='CustomerDemographics')
+
+class Customer(models.Model):
+	CompanyName = models.CharField(max_length=40)
+	ContactName = models.CharField(max_length=30)
+	# TODO.. more fields
